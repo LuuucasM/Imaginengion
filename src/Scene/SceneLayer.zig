1d18727291d98ed66874521f260a67b15f90f265
lucasmatth <lm1996kr@naver.com>
2024-10-14 06:49:19 -0400

    small changes

--- src/Scene/SceneLayer.zig
+++ src/Scene/SceneLayer.zig
@@ -22,9 +22,9 @@ pub fn CreateEntity(self: SceneLayerEditor, name: [24]u8) !Entity {
     return self.CreateEntityWithUUID(name, try GenUUID());
 }
 pub fn CreateEntityWithUUID(self: SceneLayerEditor, name: [24]u8, uuid: u64) !Entity {
-    const e = Entity{ .mEntityID = try self.mECSManager.CreateEntity(), .mLayerType = .GameLayer, .mLayer = self };
+    const e = Entity{ .mEntityID = try self.mECSManager.CreateEntity(), .mLayerType = self.mLayerType, .mLayer = self };
     _ = e.AddComponent(IDComponent, .{ .ID = uuid });
-    _ = e.AddComponent(SceneIDComponent, .{ .ID = self.mUUID });
+    _ = e.AddComponent(SceneIDComponent, .{ .ID = self.mUUID, .LayerType = self.mLayerType });
     _ = e.AddComponent(NameComponent, .{ .Name = name });
     _ = e.AddComponent(TransformComponent, .{ .Transform = LinAlg.InitMat4CompTime(1.0) });
     return e;
@@ -34,5 +34,5 @@ pub fn DestroyEntity(self: SceneLayerEditor, e: Entity) !void {
     try self.mECSManager.DestroyEntity(e.mEntityID);
 }
 pub fn DuplicateEntity(self: SceneLayerEditor, original_entity: Entity) Entity {
-    return Entity{ .mEntityID = try self.mECSManager.DuplicateEntity(original_entity.mEntityID), .mLayerType = .GameLayer, .mLayer = self };
+    return Entity{ .mEntityID = try self.mECSManager.DuplicateEntity(original_entity.mEntityID), .mLayerType = self.mLayerType, .mLayer = self };
 }
