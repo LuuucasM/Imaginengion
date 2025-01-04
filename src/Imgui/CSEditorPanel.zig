2edf9f0070e51527ec553d1aab8187271ded3ec1
LuuucasM <127158177+LuuucasM@users.noreply.github.com>
2025-01-04 17:09:08 +0900

    fixed bug of not having unique names strings for StringHashMap

--- src/Imgui/CSEditorPanel.zig
+++ src/Imgui/CSEditorPanel.zig
@@ -19,6 +19,7 @@ pub fn Init() CSEditorPanel {
 }
 
 pub fn Deinit(self: *CSEditorPanel) void {
+    //TODO: deallocate all of the keysin mEditorWindows
     self.mEditorWindows.deinit();
     EditorWindowsGPA.deinit();
 }
@@ -26,25 +27,22 @@ pub fn Deinit(self: *CSEditorPanel) void {
 pub fn OnImguiRender(self: CSEditorPanel) !void {
     if (self.mP_Open == false) return;
 
-    const p_open = true;
     const my_null_ptr: ?*anyopaque = null;
-
-    const dockspace_flags = imgui.ImGuiDockNodeFlags_None;
-    const window_flags = imgui.ImGuiWindowFlags_None;
 
     imgui.igPushStyleVar_Vec2(imgui.ImGuiStyleVar_WindowPadding, .{ .x = 0, .y = 0 });
 
-    _ = imgui.igBegin("Component/Script Editor", @ptrCast(@constCast(&p_open)), window_flags);
+    _ = imgui.igBegin("Component/Script Editor", null, 0);
     defer imgui.igEnd();
     imgui.igPopStyleVar(1);
 
     const dockspace_id = imgui.igGetID_Str("EditorDockspace");
-    _ = imgui.igDockSpace(dockspace_id, .{ .x = 0, .y = 0 }, dockspace_flags, @ptrCast(@alignCast(my_null_ptr)));
+    _ = imgui.igDockSpace(dockspace_id, .{ .x = 0, .y = 0 }, 0, @ptrCast(@alignCast(my_null_ptr)));
 
     var buffer: [300]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
     var iter = self.mEditorWindows.iterator();
     while (iter.next()) |entry| {
+        const id_name = entry.key_ptr.*;
         const window = entry.value_ptr;
 
         const entity_name = window.mEntity.GetName();
@@ -53,11 +51,12 @@ pub fn OnImguiRender(self: CSEditorPanel) !void {
         const name_len = std.mem.indexOf(u8, entity_name, &.{0}) orelse entity_name.len; // Find first null byte or use full length
         const trimmed_name = entity_name[0..name_len];
 
-        const name = try std.fmt.allocPrint(fba.allocator(), "{s} - {s}###{s}0", .{ trimmed_name, component_name, entry.key_ptr.* });
+        const name = try std.fmt.allocPrint(fba.allocator(), "{s} - {s}###{s}0", .{ trimmed_name, component_name, id_name });
         name[name.len - 1] = 0;
         defer fba.allocator().free(name);
         defer fba.reset();
-
+        //std.debug.print("name: {s}\n", .{name});
+        std.debug.print("name: {s}\n", .{id_name});
         imgui.igSetNextWindowDockID(dockspace_id, imgui.ImGuiCond_Once);
 
         _ = imgui.igBegin(name.ptr, null, 0);
@@ -79,21 +78,15 @@ pub fn OnTogglePanelEvent(self: *CSEditorPanel) void {
 }
 
 pub fn OnSelectComponentEvent(self: *CSEditorPanel, new_editor_window: EditorWindow) !void {
-    var buffer: [300]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-
-    const id_name = try std.fmt.allocPrint(fba.allocator(), "{d}{s}", .{ new_editor_window.mEntity.mEntityID, new_editor_window.GetComponentName() });
-
+    const id_name = try std.fmt.allocPrint(EditorWindowsGPA.allocator(), "{d}{s}", .{ new_editor_window.mEntity.mEntityID, new_editor_window.GetComponentName() });
+    std.debug.print("id_name: {s}\n", .{id_name});
     if (self.mEditorWindows.contains(id_name) == false) {
         try self.mEditorWindows.put(id_name, new_editor_window);
     }
 }
 
 pub fn OnSelectScriptEvent(self: *CSEditorPanel, new_editor_window: EditorWindow) !void {
-    var buffer: [300]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-
-    const id_name = try std.fmt.allocPrint(fba.allocator(), "{d}{s}", .{ new_editor_window.mEntity.mEntityID, new_editor_window.GetComponentName() });
+    const id_name = try std.fmt.allocPrint(EditorWindowsGPA.allocator(), "{d}{s}", .{ new_editor_window.mEntity.mEntityID, new_editor_window.GetComponentName() });
 
     if (self.mEditorWindows.contains(id_name) == false) {
         try self.mEditorWindows.put(id_name, new_editor_window);
